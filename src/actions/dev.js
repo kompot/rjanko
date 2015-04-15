719db43a120bc14f2d45da064919de6b5af05d28
Anton Fedchenko <kompotik@gmail.com>
2015-04-15 13:47:28 +0800

    Add debugging to `dev` action.

--- src/actions/dev.js
+++ src/actions/dev.js
@@ -1,5 +1,8 @@
 import spawnChildProcess from '../utils/spawnChildProcess';
+import dbg from 'debug';
+const debug = dbg('rjanko:actions:dev');
 
 export default function() {
+  debug(`Starting development server`);
   return spawnChildProcess('babel-node', ['server.js'], {}, true);
 }
