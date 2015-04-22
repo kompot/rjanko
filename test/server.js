9093c1363cac43c3662588c49b761fb62afb4ac7
Anton Fedchenko <kompotik@gmail.com>
2015-04-23 02:41:30 +0800

    Debugging tests on Travis.

--- test/server.js
+++ test/server.js
@@ -20,17 +20,24 @@ describe('Rjanko', function() {
     });
 
     it('should response with HTTP 200', async function(done){
+      debug(`____________1`);
       await require('../src/actions/create')({name: tmpProjectName});
+      debug(`____________2`);
       devServer = require('../src/actions/dev')({name: tmpProjectName});
+      debug(`____________3`);
       setTimeout(function() {
+        debug(`____________4`);
         axios.get('http://127.0.0.1:5000').then(function (response) {
+          debug(`____________5`);
           assert.equal(response.status, 200);
           done();
         });
-      }, 50000);
+      }, 180000);
+      debug(`____________6`);
     });
 
     after(function() {
+      debug(`____________7`);
       devServer.kill('SIGHUP');
       rimraf.sync(tmpProjectName);
     })
