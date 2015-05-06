f33d09bf1b02a20fcdff0d036fe8e3c15d9b55b5
Anton Fedchenko <kompotik@gmail.com>
2015-05-07 04:49:47 +0800

    Add `fswatch` script that propagates local file changes to `boot2docker`. To be refactored.

---
+++ templates/fswatch.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function timestamp() {
+  date +"%T"
+}
+
+function fswatch_propagate_pwd_changes_to_docker () {
+    mountDir="/xxx1"
+    localDir=$PWD
+
+    echo "Starting fswatch on $PWD -$mountDir- =$localDir="
+    # tracking previous not to get into endless loop of changing the same file
+    local previous=''
+    local previousTimestamp=$( date +%s )
+
+    fswatch -r "$PWD" | while read file; do
+        diff=$(( $( date +%s ) - $previousTimestamp ))
+
+        echo "diff = $diff"
+        if [[ $file =~ .*_jb_.* ]]
+        then
+           echo "ignore $file"
+        elif [[ "$previous" != "$file" || "$diff" -gt "1" ]]; then
+            # 40 is pwd length
+#            echo "---prev -$previous- $previousTimestamp $( date +%s )"
+            docker exec 7de4c5f4b290 touch "$mountDir/${file:40}"
+            previous="$file"
+            previousTimestamp=$( date +%s )
+            echo "Will touch $mountDir/${file:40} $previous"
+        fi
+    done
+}
+
+fswatch_propagate_pwd_changes_to_docker
