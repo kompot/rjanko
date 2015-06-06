08302d6cac197d60ca1698d6b09809e789b485c3
Anton Fedchenko <kompotik@gmail.com>
2015-06-07 06:53:22 +0800

    Gardening.

--- src/store/mongodb/index.js
+++ src/store/mongodb/index.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 import Promise from 'bluebird';
 Promise.promisifyAll(mongoose);
-import {User, UserSchema} from './User';
 
 const debug = require('core/logging/debug')(__filename);
 const derror = require('core/logging/debug')(__filename, 'error');
@@ -12,12 +11,4 @@ const db = mongoose.connection;
 db.on('error', () => derror('Error connecting to MongoDB instance.'));
 db.once('open', async () => {
   debug('Connection to MongoDB opened');
-
-  const user = new User({
-    username: 'userXX'
-  });
-
-  const saveResult = await user.saveAsync();
-  debug(`user saved ${saveResult}`);
-
 });
